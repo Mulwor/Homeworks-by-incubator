73fd56b769a086246dc8bf63090e985235caecfc
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-07-23 17:00:40 +0300

    Add files via upload

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from './Message.module.css'
+
+type messageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: messageDataType) {
+    return (
+        <div className={style.container}>
+            <div className ={style.leftSide}>
+                <img className={style.avatar} src = {props.avatar}  alt="icons"/>
+            </div>
+
+            <div className={style.rightSide}>
+                <div className={style.naming}>{props.name}</div>
+                <div className={style.message}>{props.message}</div>
+                <div className={style.timing}> {props.time} </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
